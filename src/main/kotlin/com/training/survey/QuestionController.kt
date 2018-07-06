889dc8ec1d004278eb46f15a51ecb5ccc372d3d4
pshivra <priyadarshini.shivram@transunion.com>
2018-07-06 11:25:52 -0500

    Adding a controller for the question repository

--- src/main/kotlin/com/training/survey/QuestionController.kt
+++ src/main/kotlin/com/training/survey/QuestionController.kt
@@ -1,8 +1,23 @@
 package com.training.survey
 
+import com.training.survey.bo.Question
+import com.training.survey.service.QuestionService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 
 @RestController
 class QuestionController {
+
+    @Autowired
+    lateinit var questionService : QuestionService
+
+    @GetMapping("/questions")
+    fun getAllQuestions(): List<Question> = questionService.getQuestions()
+
+    @PostMapping("/question")
+    fun save(@Valid question : Question): String = questionService.save(question)
 }
--- src/test/kotlin/com/training/survey/QuestionControllerTest.kt
+++ src/test/kotlin/com/training/survey/QuestionControllerTest.kt
@@ -8,6 +8,8 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class QuestionControllerTest{
 
@@ -23,14 +25,29 @@ class QuestionControllerTest{
         MockitoAnnotations.initMocks(this)
     }
 
-   /* @Test
+    @Test
     fun `returns all questions with id and name`(){
-        val question = Question()
-        val questions = mutableListOf(question)
-        Mockito.`when`(questionService.getQuestions()).thenReturn()
-    }*/
+        val question = Question("123","Is it fun for you")
+        val question2 = Question("125","Collaboration")
 
+        val questions = mutableListOf(question,question2)
+        Mockito.`when`(questionService.getQuestions()).thenReturn(questions)
+        val questionsResponse = questionController.getAllQuestions()
+        assertEquals("123",questionsResponse[0].id)
+        assertEquals("Is it fun for you",questionsResponse[0].name)
+        assertEquals("125",questionsResponse[1].id)
+        assertEquals("Collaboration",questionsResponse[1].name)
+    }
 
+    @Test
+    fun `saving a new question returns an id`(){
+        val question = Question("123","Is it fun for you")
+
+        Mockito.`when`(questionService.save(question)).thenReturn("123")
+        val questionsResponse = questionController.save(question)
+        assertNotNull(questionsResponse)
+        assertEquals("123",questionsResponse)
+    }
 
 
 }
